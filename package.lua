0f218fe7dc6cc58fc914e637a6d3a9e40f4d8053
squeek <squeek502@hotmail.com>
2015-04-26 16:54:33 -0700

    Change package name from "irc" to "squeek502/irc"

--- package.lua
+++ package.lua
@@ -1,8 +1,10 @@
 return {
-  name = "irc",
+  name = "squeek502/irc",
   version = "0.3.0",
   description = "irc client module for luvit",
-  author = "squeek",
+  author = {
+    name="squeek"
+  },
   dependencies = {
     "luvit/luvit@2.1.0"
   },
